7a65f1c583354438fb05d208522fcbba138deaf3
Spardoksis <vitasman122@gmail.com>
2024-08-20 00:14:34 +0300

    Add task implementation

--- stations/views.py
+++ stations/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, redirect
+import csv
+
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
 from django.urls import reverse
+
+from pagination.settings import BUS_STATION_CSV
+
+CONTENT = []
+with open(BUS_STATION_CSV, encoding='UTF-8') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        CONTENT.append(
+            {
+                'Name': row['Name'],
+                'Street': row['Street'],
+                'District': row['District']
+            }
+        )
 
 
 def index(request):
@@ -7,11 +24,13 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    page_number = int(request.GET.get("page", 1))
+
+    paginator = Paginator(CONTENT, 10)
+    stations = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': stations,
+        'page': stations,
     }
     return render(request, 'stations/index.html', context)
